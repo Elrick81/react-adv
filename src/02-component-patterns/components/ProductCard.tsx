baffabb4acf4103c91c9b473ea0ba12e1840c2b7
Christian <christian.alos.segui@gmail.com>
2022-01-25 17:26:08 +0100

    control props implementado

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactElement, useContext } from 'react';
 
 import { useProduct } from '../hooks/useProduct';
-import { Product, ProductContexProps } from '../interfaces/interfaces';
+import { onChangeArgs, Product, ProductContexProps } from '../interfaces/interfaces';
 import styles from '../styles/styles.module.css';
 
 
@@ -15,13 +15,15 @@ export interface Props {
     product: Product;
     children?: ReactElement | ReactElement[];
     className?: string;
-    style?: React.CSSProperties
+    style?: React.CSSProperties;
+    onChange?: (args: onChangeArgs) => void;
+    value?: number;
 
 }
 
 
 
-export const ProductCard = ({ children, product, className, style } : Props) => {
+export const ProductCard = ({ children, product, className, style, onChange, value } : Props) => {
     
     /*
     const [ counter, setCounter ] = useState(0)
@@ -30,7 +32,7 @@ export const ProductCard = ({ children, product, className, style } : Props) => 
         setCounter( prev => Math.max(prev + value, 0));
     }
     */
-    const { counter, increaseBy } = useProduct();
+    const { counter, increaseBy } = useProduct({ onChange, product, value });
 
 
     return(
